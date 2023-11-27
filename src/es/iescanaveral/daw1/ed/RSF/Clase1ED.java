2d2d08e8318c012db2dac80b11b4688af4db6d9d
RafaelSoria1993 <150554539+RafaelSoria1993@users.noreply.github.com>
2023-11-27 11:08:27 +0100

    Update de prueba Clase1ED.java
    Probando cambios desde github

--- src/es/iescanaveral/daw1/ed/RSF/Clase1ED.java
+++ src/es/iescanaveral/daw1/ed/RSF/Clase1ED.java
@@ -4,6 +4,7 @@ public class Clase1ED {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Haciendo pruebas para eclipse
 		Clase2ED clase2 = new Clase2ED();
 		clase2.Saludar("RSF");
 
